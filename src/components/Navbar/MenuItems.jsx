29dc96ea9a3137d67d76e1e9eb83f74bb2e8769e
Hayk <hayknurijanyan@gmail.com>
2021-02-28 09:39:41 -0800

    first commit

---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,48 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Products",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Ideas",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "App",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "How To Use",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Delivery",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "About Us",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Contacts",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Order Now",
+        url: "#",
+        cName: "nav-links"
+    }
+]
+

be9d93ce70a8f7f78313e23a0e3e58d15762c3ef
Hayk <hayknurijanyan@gmail.com>
2021-03-30 21:55:30 +0400

    firebase added

---
+++ src/components/Authentication/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+const SignUp = () => {
+  return (
+    <>
+      <Card></Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log in
+      </div>
+    </>
+  );
+};
+
+export default SignUp;

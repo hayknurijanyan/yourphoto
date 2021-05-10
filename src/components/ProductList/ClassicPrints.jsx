027ee8c129156fbae0b122f8e47302e6f2b59467
Hayk <hayknurijanyan@gmail.com>
2021-05-10 14:14:59 +0400

    some css changes

--- src/components/ProductList/ClassicPrints.jsx
+++ src/components/ProductList/ClassicPrints.jsx
@@ -14,43 +14,45 @@ const ClassicPrints = () => {
   };
 
   return (
-    <div
-      id="classic-prints"
-      // style={{
-      //   display: "grid",
-      //   justifyContent: "center",
-      //   gridGap: "20px",
-      //   gridTemplateColumns: "repeat(4, minmax(230px, auto))",
-      //   gridTemplateRows: "repeat(2, minmax(230px, auto))",
-      //   marginBottom: "50px",
-      // }}
-    >
-      {ClassicPrintsData.map((el, i) => {
-        return (
-          <Link
-            key={i}
-            onClick={() => handleDefaultSize(el)}
-            to={`order/${el.size}`}
-            className={el.cName}>
-            <div
-              className="box-details"
-              // style={{
-              //   display: "flex",
-              //   flexDirection: "column",
-              //   justifyContent: "flex-end",
-              //   width: "100%",
-              //   height: "100%",
-              // }}
-            >
-              <div className="box-detail-item">
-                <span>{el.size}</span>
-                <span>{el.price} AMD</span>
+    <>
+      <div
+        id="classic-prints"
+        // style={{
+        //   display: "grid",
+        //   justifyContent: "center",
+        //   gridGap: "20px",
+        //   gridTemplateColumns: "repeat(4, minmax(230px, auto))",
+        //   gridTemplateRows: "repeat(2, minmax(230px, auto))",
+        //   marginBottom: "50px",
+        // }}
+      >
+        {ClassicPrintsData.map((el, i) => {
+          return (
+            <Link
+              key={i}
+              onClick={() => handleDefaultSize(el)}
+              to={`order/${el.size}`}
+              className={el.cName}>
+              <div
+                className="box-details"
+                // style={{
+                //   display: "flex",
+                //   flexDirection: "column",
+                //   justifyContent: "flex-end",
+                //   width: "100%",
+                //   height: "100%",
+                // }}
+              >
+                <div className="box-detail-item">
+                  <span>{el.size}</span>
+                  <span>{el.price} AMD</span>
+                </div>
               </div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
